b07779368809e1386a6eb66dc6c88e5cd149ca1c
wep1980 <wepbike@gmail.com>
2022-02-10 22:08:53 -0300

    projeto iniciado

---
+++ src/test/java/br/com/wepdev/apitest/ApiTestApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.wepdev.apitest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiTestApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
