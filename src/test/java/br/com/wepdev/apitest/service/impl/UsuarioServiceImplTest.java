30933043f64b03c0d8db81d95ce02293bebbe9b4
wep1980 <wepbike@gmail.com>
2022-02-12 11:13:12 -0300

    Teste para classe UsuarioServiceImpl create() sucesso!

--- src/test/java/br/com/wepdev/apitest/service/impl/UsuarioServiceImplTest.java
+++ src/test/java/br/com/wepdev/apitest/service/impl/UsuarioServiceImplTest.java
@@ -94,7 +94,18 @@ class UsuarioServiceImplTest {
     }
 
     @Test
-    void create() {
+    void deveriaCriarUmUsuario() {
+        Mockito.when(repository.save(Mockito.any())).thenReturn(usuarioUm); // Resposta mockada
+
+        Usuario response = service.create(usuarioDTO); // Chamando o metodo create do service
+
+        assertNotNull(response); // Verifica se a resposta não e nula
+        assertEquals(Usuario.class, response.getClass()); // Verifica se o objeto e do tipo Usario
+        Assertions.assertEquals(ID, response.getId()); // Assegura que o ID passado e o mesmo Id do response
+        Assertions.assertEquals(NOME, response.getNome()); // Assegura que o nome passado e o mesmo nome do response
+        Assertions.assertEquals(EMAIL, response.getEmail()); // Assegura que o email passado e o mesmo email do response
+        Assertions.assertEquals(SENHA, response.getSenha()); // Assegura que a senha passado e a mesmo senha do response
+
     }
 
     @Test
