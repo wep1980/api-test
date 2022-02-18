43a5ca465f0c694b5133c75d54d87dfd57d84615
wep1980 <wepbike@gmail.com>
2022-02-17 22:37:00 -0300

    Testes Controllers, Projeto Finalizado

--- src/test/java/br/com/wepdev/apitest/controller/UsuarioControllerTest.java
+++ src/test/java/br/com/wepdev/apitest/controller/UsuarioControllerTest.java
@@ -12,9 +12,12 @@ import org.mockito.Mockito;
 import org.mockito.MockitoAnnotations;
 import org.modelmapper.ModelMapper;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 
-import java.util.Optional;
+import java.util.ArrayList;
+import java.util.List;
+
 
 import static org.junit.jupiter.api.Assertions.*;
 
@@ -65,18 +68,62 @@ class UsuarioControllerTest {
 
     @Test
     void findAll() {
+        Mockito.when(service.findAll()).thenReturn(List.of(usuario));
+        Mockito.when(mapper.map(Mockito.any(), Mockito.any())).thenReturn(usuarioDTO);
+
+        ResponseEntity<List<UsuarioDTO>> response = controller.findAll();
+
+        assertNotNull(response);
+        assertNotNull(response.getBody());
+        assertEquals(HttpStatus.OK, response.getStatusCode());
+        assertEquals(ResponseEntity.class, response.getClass());
+        assertEquals(ArrayList.class, response.getBody().getClass());
+        assertEquals(UsuarioDTO.class, response.getBody().get(0).getClass());
+
+        assertEquals(ID, response.getBody().get(0).getId());
+        assertEquals(NOME, response.getBody().get(0).getNome());
+        assertEquals(EMAIL, response.getBody().get(0).getEmail());
+        assertEquals(SENHA, response.getBody().get(0).getSenha());
     }
 
     @Test
-    void create() {
+    void testCreate() {
+        Mockito.when(service.create(Mockito.any())).thenReturn(usuario);
+        ResponseEntity<UsuarioDTO> response = controller.create(usuarioDTO);
+
+        Assertions.assertEquals(ResponseEntity.class, response.getClass()); // Assegura que o corpo da resposta seja igual ao do response, que é ResponseEntity
+        Assertions.assertEquals(HttpStatus.CREATED, response.getStatusCode());
+        Assertions.assertNotNull(response.getHeaders().get("Location"));
     }
 
     @Test
-    void update() {
+    void testUpdate() {
+        Mockito.when(service.update(usuarioDTO)).thenReturn(usuario);
+        Mockito.when(mapper.map(Mockito.any(), Mockito.any())).thenReturn(usuarioDTO);
+
+        ResponseEntity<UsuarioDTO> response = controller.update(ID, usuarioDTO);
+
+        assertNotNull(response);
+        assertNotNull(response.getBody());
+        assertEquals(HttpStatus.OK, response.getStatusCode());
+        assertEquals(ResponseEntity.class, response.getClass());
+        assertEquals(UsuarioDTO.class, response.getBody().getClass());
+
+        assertEquals(ID, response.getBody().getId());
+        assertEquals(NOME, response.getBody().getNome());
+        assertEquals(EMAIL, response.getBody().getEmail());
     }
 
     @Test
     void delete() {
+        Mockito.doNothing().when(service).delete(Mockito.anyLong());
+
+        ResponseEntity<UsuarioDTO> response = controller.delete(ID);
+
+        assertNotNull(response);
+        assertEquals(ResponseEntity.class, response.getClass());
+        assertEquals(HttpStatus.NO_CONTENT, response.getStatusCode());
+        Mockito.verify(service, Mockito.times(1)).delete(Mockito.anyLong());
     }
 
     private void iniciarUsuario(){
